8443dc3043ed2d52faf445b6c4ea215bfe6d735a
Javed Khan <tuxcanfly@gmail.com>
2022-09-16 04:14:51 +0200

    fix image

---
+++ clients/op-indexer/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Hive requires us to prefix env vars with "HIVE_"
+# Iterate the env, find all HIVE_UNPACK_ vars, and remove the HIVE_UNPACK_ prefix.
+while IFS='=' read -r -d '' n v; do
+    if [[ "$n" == HIVE_UNPACK_* ]]; then
+        name=${n#"HIVE_UNPACK_"}  # remove the HIVE_UNPACK_ prefix
+        echo "$name=$v"
+        declare -gx "$name=$v"
+    fi
+done < <(env -0)
+
+indexer
+
+
