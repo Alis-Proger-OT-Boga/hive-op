75e1add92c2ca8d2036c6a651f0e4503627186ee
Adrian Sutton <adrian@oplabs.co>
2023-03-10 15:34:31 +1000

    Tidy up

--- junit/junitformatter.go
+++ junit/junitformatter.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"os"
 
@@ -11,7 +12,7 @@ import (
 
 func main() {
 	if len(os.Args) <= 1 {
-		fail("No input files specified")
+		fail(errors.New("no input files specified"))
 	}
 
 	result := TestSuites{
@@ -22,21 +23,27 @@ func main() {
 	var suites []TestSuite
 
 	for i := 1; i < len(os.Args); i++ {
-		// TODO: Read multiple input files
 		inData, err := os.ReadFile(os.Args[i])
 
 		if err != nil {
-			failErr(err)
+			fail(err)
 		}
 		var suite libhive.TestSuite
 		err = json.Unmarshal(inData, &suite)
 		if err != nil {
-			failErr(err)
+			fail(err)
 		}
 		junitSuite := TestSuite{
-			Name:     suite.Name,
-			Failures: 0,
-			Tests:    len(suite.TestCases),
+			Name:       suite.Name,
+			Failures:   0,
+			Tests:      len(suite.TestCases),
+			Properties: Properties{},
+		}
+		for clientName, clientVersion := range suite.ClientVersions {
+			junitSuite.Properties.Properties = append(junitSuite.Properties.Properties, Property{
+				Name:  clientName,
+				Value: clientVersion,
+			})
 		}
 		for _, testCase := range suite.TestCases {
 			if !testCase.SummaryResult.Pass {
@@ -46,7 +53,7 @@ func main() {
 				Name: testCase.Name,
 			}
 			if testCase.SummaryResult.Pass {
-				//junitCase.SystemOut = testCase.SummaryResult.Details
+				junitCase.SystemOut = testCase.SummaryResult.Details
 			} else {
 				junitCase.Failure = &Failure{Message: testCase.SummaryResult.Details}
 			}
@@ -60,48 +67,18 @@ func main() {
 
 	junit, err := xml.MarshalIndent(result, "", "  ")
 	if err != nil {
-		failErr(err)
+		fail(err)
 	}
 	fmt.Println(string(junit))
 }
 
-func fail(reason string) {
-	fmt.Println(reason)
-	os.Exit(2)
-}
-
-func failErr(reason error) {
+func fail(reason error) {
 	fmt.Println(reason)
 	os.Exit(1)
 }
 
-type TestSuites struct {
-	XMLName  string      `xml:"testsuites,omitempty"`
-	Failures int         `xml:"failures,attr"`
-	Name     string      `xml:"name,attr"`
-	Tests    int         `xml:"tests,attr"`
-	Suites   []TestSuite `xml:"testsuite"`
-}
-
-type TestSuite struct {
-	Name      string     `xml:"name,attr"`
-	Failures  int        `xml:"failures,attr"`
-	Tests     int        `xml:"tests,attr"`
-	TestCases []TestCase `xml:"testcase"`
-	// TODO: Include client versions as properties?
-}
-
-type TestCase struct {
-	Name      string   `xml:"name,attr"`
-	Failure   *Failure `xml:"failure,omitempty"`
-	SystemOut string   `xml:"system-out,omitempty"`
-}
-
-type Failure struct {
-	Message string `xml:"message,attr"`
-}
-
 /*
+Target XML format (lots of it being optional):
 <testsuites disabled="" errors="" failures="" name="" tests="" time="">
     <testsuite disabled="" errors="" failures="" hostname="" id=""
                name="" package="" skipped="" tests="" time="" timestamp="">
@@ -120,3 +97,38 @@ type Failure struct {
     </testsuite>
 </testsuites>
 */
+
+type TestSuites struct {
+	XMLName  string      `xml:"testsuites,omitempty"`
+	Failures int         `xml:"failures,attr"`
+	Name     string      `xml:"name,attr"`
+	Tests    int         `xml:"tests,attr"`
+	Suites   []TestSuite `xml:"testsuite"`
+}
+
+type TestSuite struct {
+	Name       string     `xml:"name,attr"`
+	Failures   int        `xml:"failures,attr"`
+	Tests      int        `xml:"tests,attr"`
+	Properties Properties `xml:"properties,omitempty"`
+	TestCases  []TestCase `xml:"testcase"`
+}
+
+type TestCase struct {
+	Name      string   `xml:"name,attr"`
+	Failure   *Failure `xml:"failure,omitempty"`
+	SystemOut string   `xml:"system-out,omitempty"`
+}
+
+type Failure struct {
+	Message string `xml:"message,attr"`
+}
+
+type Properties struct {
+	Properties []Property `xml:"property,omitempty"`
+}
+
+type Property struct {
+	Name  string `xml:"name,attr"`
+	Value string `xml:"value,attr"`
+}
